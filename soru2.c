ac02f909548948711429bc8135183c2c15a7a8a0
sarana200 <47283850+sarana200@users.noreply.github.com>
2020-11-05 13:53:13 +0300

    Update soru2.c

--- soru2.c
+++ soru2.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    int x;
-    //int Array[]={};
-    int r;
-    for (int i=100; i<=400; ++i){
+    int x; //temp icinde verinin kopyasi saklanir
+    int r; //reminder value;
+    for (int i=100; i<=400; ++i){ //100 -400 arasındaki sayılar ,400 dahil eetim
         x=i;
-        if(i%2==0){
-            while(x!=0){
-                r=x%10;
-                if(r%2==0){
+        if(i%2==0){ //Çift olan sayılar içeriye girir
+            while(x!=0){ //her digiti konrtol ediyor çift mi değil mi
+                r=x%10; //digit digit sayısı parçalar
+                if(r%2==0){ //her digitin tek mi çiftmi kontrol eder
                     x=x/10;
-                }else{
+                }else{ //eğer bir digit tek çıkarsa looptan çıkar 
                     break;
                 }
             }
-            if(x==0){
+            if(x==0){ //eğer adımları tamamiyle bitirmiş ise demekki çifttir
+            // bu yüzden i de saklanan valueyu prıntf eder 
                 printf("%d ",i);
             }
         }
     }
-    
- return 0;
+        
+return 0;
 }
